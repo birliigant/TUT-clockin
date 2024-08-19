84a3cd3f46f1ce6c37f47a21e2e6c9be4d49797a
ze <ze@vv.com>
2024-08-19 21:01:17 +0800

    [feat#]查询个人信息

--- src/main/java/com/sipc/clockin/mapper/StudentMapper.java
+++ src/main/java/com/sipc/clockin/mapper/StudentMapper.java
@@ -1,6 +1,7 @@
 package com.sipc.clockin.mapper;
 
 import cn.hutool.core.date.DateTime;
+import com.sipc.clockin.pojo.domain.DO.UserInfo;
 import com.sipc.clockin.pojo.domain.PO.Clock;
 import com.sipc.clockin.pojo.model.request.ClockRequest;
 import com.sipc.clockin.pojo.model.result.StudentClockDetail;
@@ -11,6 +12,11 @@ import java.util.List;
 @Mapper
 public interface StudentMapper {
     List<Clock> getClockRecord(Integer id); //左下角获取打卡记录
+
     Integer addClock(ClockRequest request); //打卡
-    List<StudentClockDetail> getClassClock(Integer workId,DateTime date); //获取班级打卡信息
+
+    List<StudentClockDetail> getClassClock(Integer workId, DateTime date); //获取班级打卡信息
+
+    //查询学生信息
+    UserInfo getUserById(Integer id);
 }
