2f3def0b3be4ea02f3e9340b73dda2be11f38806
birliigant <birliigantJesus@outlook.com>
2024-08-19 10:54:56 +0800

    [feat#] 为辅导员controller添加mapping映射

--- src/main/java/com/sipc/clockin/controller/TeacherController.java
+++ src/main/java/com/sipc/clockin/controller/TeacherController.java
@@ -9,9 +9,7 @@ import com.sipc.clockin.pojo.model.request.UpdateTeacherRequest;
 import com.sipc.clockin.pojo.model.result.BlankResult;
 import com.sipc.clockin.service.TeacherService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,24 +18,31 @@ import java.util.List;
 @RequestMapping("/teacher")
 public class TeacherController {
     private TeacherService teacherService;
+    @GetMapping("/myself")
     CommonResult<TeacherInfo> queryTeacherInfo(){
         return teacherService.queryTeacherInfo();
     }
+    @PutMapping("/update")
     CommonResult<BlankResult> updateTeacherInfo(UpdateTeacherRequest request){
         return teacherService.updateTeacherInfo(request);
     }
+    @PostMapping("/class/add")
     CommonResult<BlankResult> addClass(ClassRequest request){
         return teacherService.addClass(request);
     }
+    @PostMapping("/class/delete")
     CommonResult<BlankResult> deleteClass(ClassRequest request){
         return teacherService.deleteClass(request);
     }
+    @PostMapping("/manage/add")
     CommonResult<BlankResult> addManager(ManageRequest request){
         return teacherService.addManager(request);
     }
+    @PostMapping("/manage/delete")
     CommonResult<BlankResult> deleteManager(ManageRequest request){
         return teacherService.deleteManager(request);
     }
+    @GetMapping("/class")
     CommonResult<List<UserSimple>> selectStudents(@RequestParam("class_id") int classId){
         return teacherService.selectStudents(classId);
     }
